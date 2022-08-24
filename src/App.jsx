2683af22987636559210575bd90bd1f989bbe199
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-08-24 16:59:56 +0200

    Feat: create browser and Home container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./containers/Home/Home"
 import './App.scss'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+import "./Home.scss"
+
+const Home = () => {
+
+   return(
+      <div id="Home">
+         <div className="wellcomeMessageBox">
+            <p>HOME PAGE</p>
+         </div>
+      </div>
+   )
+}
+
+export default Home;
