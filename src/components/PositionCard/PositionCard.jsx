5a64a003f5ebea362cc6dd985a685ab876e29fda
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-06 12:22:30 +0200

    Feat: edit PositionCard to show some position details

--- src/components/PositionCard/PositionCard.jsx
+++ src/components/PositionCard/PositionCard.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import './PositionCard.scss'
 
 const PositionCard = (props) => {
-    console.log("llego")
+    console.log(props.data)
 
-    return(
+    const goToPosition = () => {
+        return
+    }
+
+    return (
         <div className="PositionCard">
-            <p className="cardText">{props.data.title}</p>
+            <p className="cardTitle">{props.data.title}</p>
+            <p className="cardCompany">{props.data.company.name.toUpperCase()}</p>
+            <p className="cardText">Published: {new Date(props?.data.created_at).toLocaleDateString()}</p>
+            <p className="cardText">{props.data.location} | {props.data.mode}</p>
+            <p className="cardText">Salary: {props.data.salary}</p>
+            <button id="detailsButton" onClick={goToPosition}>See details</button>
         </div>
     )
 }
