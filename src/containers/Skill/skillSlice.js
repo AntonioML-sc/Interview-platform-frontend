865beac506abd48692c453a91ee47a6e5f463708
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-07 20:07:53 +0200

    Feat(redux): create skillSlice and register skill function inside

---
+++ src/containers/Skill/skillSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const skillSlice = createSlice({
+   name: "skill",
+   initialState: {
+      skill: "",
+      skillList: []
+   },
+   reducers: {
+      setSkill: (state, action) => {
+         state.position = action.payload
+      },
+      setSkillList: (state, action) => {
+         state.position = action.payload
+      },
+      register: (state, action) => {
+         return {
+            ...state,
+            isError: false,
+            successMessage: 'Skill registered successfully',
+            errorMessage: ''
+         }
+      },
+      logError: (state, action) => {
+         return {
+            ...state,
+            isError: !action.payload.success,
+            errorMessage: action.payload.message,
+            successMessage: '',
+         }
+      }
+   }
+})
+
+export const registerSkill = (title, description, token) => async (dispatch) => {
+   try {
+      const body = {
+         title: title,
+         description: description
+      }
+      const config = {
+         headers: { "Authorization": `Bearer ${token}` }
+      }
+      const skill = await axios.post('https://aml-mysql-08-18-22-laravel-ip.herokuapp.com/api/companies/new', body, config);
+
+      let response = skill;
+      if (response.status === 201 || response.status === 200) {
+         dispatch(register());
+      }
+   } catch (error) {
+      dispatch(logError(error));
+   }
+};
+
+export const { setSkill, setSkillList, logError, register } = skillSlice.actions
+
+export const selectSkill = (state) => state.skill.skill
+export const selectSkillList = (state) => state.skill.skillList
+
+export default skillSlice.reducer
