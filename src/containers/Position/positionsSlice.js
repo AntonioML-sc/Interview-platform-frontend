4f0023e637e54f4b871b4b9fdd0189b2940210fd
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-07 11:34:51 +0200

    Feat: create function to register position in positionSlice

--- src/containers/Position/positionsSlice.js
+++ src/containers/Position/positionsSlice.js
@@ -12,9 +12,33 @@ export const positionsSlice = createSlice({
         },
         setPositionsList: (state, action) => {
             state.positionsList = action.payload
+        },
+        logError: (state, action) => {
+            return {
+                ...state,
+                isError: !action.payload.success,
+                errorMessage: action.payload.message,
+                successMessage: '',
+            }
         }
     }
 })
+
+export const registerPosition = (title, description, companyName, location, mode, salary) => async (dispatch) => {
+    try {
+        const position = await axios.post('https://aml-mysql-08-18-22-laravel-ip.herokuapp.com/api/positions/new',
+            {
+                title: title,
+                description: description,
+                company_name: companyName,
+                location: location,
+                mode: mode,
+                salary: salary
+            });
+    } catch (error) {
+        dispatch(logError(error));
+    }
+};
 
 export const { setPosition, setPositionsList } = positionsSlice.actions
 
