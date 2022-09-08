86285ee81af7680a77d42ea3f00451a2428f673c
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-08 19:12:45 +0200

    Feat(create): render all skills from api on loading CreatePosition component

--- src/containers/Position/CreatePosition/CreatePosition.jsx
+++ src/containers/Position/CreatePosition/CreatePosition.jsx
@@ -9,137 +9,192 @@ import { registerPosition, selectPosition } from "../positionsSlice";
 import './CreatePosition.scss'
 
 const CreatePosition = () => {
-    const userInfo = useSelector(userData)
-    const positionsInfo = useSelector(selectPosition)
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const recruiterRoleId = "5695fbbd-4675-4b2a-b31d-603252c21c94"
+   const userInfo = useSelector(userData)
+   const positionsInfo = useSelector(selectPosition)
+   const navigate = useNavigate()
+   const dispatch = useDispatch()
+   const recruiterRoleId = "5695fbbd-4675-4b2a-b31d-603252c21c94"
 
-    // data related to skill search
-    let [skillLists, setSkillLists] = useState({
-        userSkillList: [],
-        otherSkillList: [],
-        searchWord: ""
-    })
+   // data related to skill search
+   let [skillLists, setSkillLists] = useState({
+      positionSkillList: [],
+      otherSkillList: [],
+      searchWord: ""
+   })
 
-    useEffect(() => {
-        if (!userInfo?.data || userInfo?.data.role_id != recruiterRoleId) {
-            navigate('/')
-        }
-        const fetchSkills = async () => {
-            await axios.get('https://aml-mysql-08-18-22-laravel-ip.herokuapp.com/api/skills/get-all')
-                .then(resp => {
-                    setSkillLists({
-                        ...skillLists,
-                        otherSkillList: resp.data
-                    })
-                }).then(resp => {
-                    console.log(skillLists.otherSkillList)
-                }).catch((error) => {
-                    console.log(error)
-                })
-        }
-        fetchSkills()
-    }, [])
+   // data to register position
+   const [register, setRegister] = useState({
+      title: '',
+      description: '',
+      companyName: '',
+      location: '',
+      mode: '',
+      salary: '',
+      isError: false,
+      message: ''
+   })
 
-    const [register, setRegister] = useState({
-        title: '',
-        description: '',
-        companyName: '',
-        location: '',
-        mode: '',
-        salary: '',
-        isError: false,
-        message: ''
-    })
+   useEffect(() => {
+      if (!userInfo?.data || userInfo?.data.role_id != recruiterRoleId) {
+         navigate('/')
+      }
+      const fetchSkills = async () => {
+         await axios.get('https://aml-mysql-08-18-22-laravel-ip.herokuapp.com/api/skills/get-all')
+            .then(resp => {
+               setSkillLists({
+                  ...skillLists,
+                  otherSkillList: resp.data
+               })
+            }).then(resp => {
+               console.log(skillLists.otherSkillList)
+            }).catch((error) => {
+               console.log(error)
+            })
+      }
+      fetchSkills()
+   }, [])
 
-    // handler to update hook with info from form fields
-    const handleInput = (event) => {
-        setRegister({
+   // handler to update hook with info from form fields
+   const handleInput = (event) => {
+      setRegister({
+         ...register,
+         [event.target.name]: event.target.value
+      })
+   }
+
+   // test if the info stored in hook is correct and register position in that case
+   const positionRegister = async (event) => {
+      event.preventDefault()
+
+      // function to set an error with custon message in register hook
+      const setRegisterError = (value, message) => {
+         setRegister({
             ...register,
-            [event.target.name]: event.target.value
-        })
-    }
-    // test if the info stored in hook is correct and register position in that case
-    const positionRegister = async (event) => {
-        event.preventDefault()
+            isError: value,
+            message: message
+         });
+      }
 
-        // function to set an error with custon message in register hook
-        const setRegisterError = (value, message) => {
-            setRegister({
-                ...register,
-                isError: value,
-                message: message
-            });
-        }
+      // form inputs to validate
+      const validations = [
+         ['title', register.title, 'Invalid title format'],
+         ['company_name', register.companyName, 'Invalid company name format'],
+         ['location', register.location, 'Invalid location format'],
+         ['mode', register.mode, 'Invalid mode format'],
+         ['salary', register.salary, 'Invalid salary format'],
+      ]
 
-        // form inputs to validate
-        const validations = [
-            ['title', register.title, 'Invalid title format'],
-            ['company_name', register.companyName, 'Invalid company name format'],
-            ['location', register.location, 'Invalid location format'],
-            ['mode', register.mode, 'Invalid mode format'],
-            ['salary', register.salary, 'Invalid salary format'],
-        ]
+      // apply evals and register position if everything is ok
+      for (let index in validations) {
+         if (!evalField(validations[index][0], validations[index][1])) {
+            setRegisterError(true, validations[index][2])
+            return
+         } else if (index == validations.length - 1) {
+            setRegisterError(false, '')
+            const userToken = userInfo?.token
+            dispatch(registerPosition(register.title, register.description, register.companyName, register.location, register.mode, register.salary, userToken))
+            setTimeout(() => navigate("/"), 3000)
+         }
+      }
+   }
 
-        // apply evals and register position if everything is ok
-        for (let index in validations) {
-            if (!evalField(validations[index][0], validations[index][1])) {
-                setRegisterError(true, validations[index][2])
-                return
-            } else if (index == validations.length - 1) {
-                setRegisterError(false, '')
-                const userToken = userInfo?.token
-                dispatch(registerPosition(register.title, register.description, register.companyName, register.location, register.mode, register.salary, userToken))
-                setTimeout(() => navigate("/"), 3000)
-            }
-        }
-    }
+   // renders otherSkillList
+   const OtherSkillList = () => {
+      if(!skillLists.otherSkillList.data) {
+         
+         console.log("no llega")
+         return (
+            <div></div>
+         )
+      } else {
+         console.log(skillLists.otherSkillList)
 
-    return (
-        <div id="CreatePosition">
-            <div className="mainBox">
-                <p>Create new position</p>
-                <form onSubmit={positionRegister}>
-                    <div className="registerItem">
-                        <label className="registerLabel">Title</label>
-                        <input className="registerInput" onChange={handleInput} type="text" name="title" />
-                    </div>
+         if (skillLists.otherSkillList?.data.length > 0) {
 
-                    <div className="registerItem">
-                        <label className="registerLabel">Company name</label>
-                        <input className="registerInput" onChange={handleInput} type="text" name="companyName" />
-                    </div>
+            return (
+               skillLists.otherSkillList.data.map((skill, index) => (
+                  <p key={index} className="skillTag">{skill.title}</p>
+               ))
+            )
+         } else {
+   
+            console.log("no mapea")
+            return (
+               <div></div>
+            )
+         }
+      }
+   }
 
-                    <div className="registerItem">
-                        <label className="registerLabel">Location</label>
-                        <input className="registerInput" onChange={handleInput} type="text" name="location" />
-                    </div>
+   // handler for skill search
+   const handleChange = (event) => {
+      return
+   }
 
-                    <div className="registerItem">
-                        <label className="registerLabel">Mode</label>
-                        <input className="registerInput" onChange={handleInput} type="text" name="mode" />
-                    </div>
+   return (
+      <div id="CreatePosition">
+         <div className="mainBox">
+            <p>Create new position</p>
+            <form onSubmit={positionRegister}>
+               <div className="registerItem">
+                  <label className="registerLabel">Title</label>
+                  <input className="registerInput" onChange={handleInput} type="text" name="title" />
+               </div>
 
-                    <div className="registerItem">
-                        <label className="registerLabel">Salary</label>
-                        <input className="registerInput" onChange={handleInput} type="text" name="salary" />
-                    </div>
+               <div className="registerItem">
+                  <label className="registerLabel">Company name</label>
+                  <input className="registerInput" onChange={handleInput} type="text" name="companyName" />
+               </div>
 
-                    <div className="registerItem">
-                        <label className="registerLabel">Description</label>
-                        <input className="registerInput" onChange={handleInput} type="text" name="description" />
-                    </div>
+               <div className="registerItem">
+                  <label className="registerLabel">Location</label>
+                  <input className="registerInput" onChange={handleInput} type="text" name="location" />
+               </div>
 
-                    <div className="registerItem">
-                        <button className="registerSubmit" type="submit">Register</button>
-                    </div>
-                </form>
-                <p className="errorMessage">{register.isError ? register.message : ''}</p>
-                <p className="errorMessage">{positionsInfo.isError ? positionsInfo.errorMessage : positionsInfo.successMessage}</p>
+               <div className="registerItem">
+                  <label className="registerLabel">Mode</label>
+                  <input className="registerInput" onChange={handleInput} type="text" name="mode" />
+               </div>
+
+               <div className="registerItem">
+                  <label className="registerLabel">Salary</label>
+                  <input className="registerInput" onChange={handleInput} type="text" name="salary" />
+               </div>
+
+               <div className="registerItem">
+                  <label className="registerLabel">Description</label>
+                  <input className="registerInput" onChange={handleInput} type="text" name="description" />
+               </div>
+
+               <div className="registerItem">
+                  <p className="registerLabel">Skills required</p>
+               </div>
+
+               <div className="registerItem">
+                  <button className="registerSubmit" type="submit">Register</button>
+               </div>
+            </form>
+
+            <div className="registerItem">
+               <p className="registerLabel">Click to add skills</p>
             </div>
-        </div>
-    )
+
+            <div className="otherSkillContainer">
+               <div className="searchBar">
+                  <form className="searchBarForm">
+                     <input className="inputBox" type="text" name="searchWord" placeholder=" Search"></input>
+                  </form>
+               </div>
+               <div className="otherSkillList">
+                  <OtherSkillList />
+               </div>
+            </div>
+
+            <p className="errorMessage">{register.isError ? register.message : ''}</p>
+            <p className="errorMessage">{positionsInfo.isError ? positionsInfo.errorMessage : positionsInfo.successMessage}</p>
+         </div>
+      </div>
+   )
 }
 
 export default CreatePosition
