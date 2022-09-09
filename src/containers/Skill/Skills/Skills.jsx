609042fa015fcab7abed8634c3f61ffb4d184fa6
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-09 14:21:15 +0200

    Feat(get): add hook to retrieve all skills on loading page

--- src/containers/Skill/Skills/Skills.jsx
+++ src/containers/Skill/Skills/Skills.jsx
@@ -1,9 +1,40 @@
 
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { logError, selectSkillList, setSkillList } from "../skillSlice"
 import "./Skills.scss"
 
 const Skills = () => {
+   const dispatch = useDispatch()
+   const skillList = useSelector(selectSkillList)
 
-   return(
+   let [skillsData, setSkillsData] = useState({
+      skillSearchList: [],
+      searchword: ""
+   })
+
+   useEffect(() => {
+      const fetchSkills = async () => {
+         if (skillList.length == 0) {
+            await axios.get('https://aml-mysql-08-18-22-laravel-ip.herokuapp.com/api/skills/get-all')
+               .then(resp => {
+                  dispatch(setSkillList(resp.data.data))
+                  setSkillsData({
+                     ...skillsData,
+                     skillSearchList: resp.data.data
+                  })
+               }).catch(error => {
+                  dispatch(logError(error))
+               })
+         }
+      }
+      fetchSkills()
+   }, [])
+
+   console.log(skillsData.skillSearchList)
+
+   return (
       <div id="Skills">
          <div className="wellcomeMessageBox">
             <p>SKILLS PAGE</p>
