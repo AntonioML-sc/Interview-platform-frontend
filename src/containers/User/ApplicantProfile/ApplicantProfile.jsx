15c0e5d291d65a62446f6885a26dcf576e5ec9f9
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-10 19:57:04 +0200

    Feat(applicants): add user's applications to my profile

--- src/containers/User/ApplicantProfile/ApplicantProfile.jsx
+++ src/containers/User/ApplicantProfile/ApplicantProfile.jsx
@@ -1,7 +1,9 @@
 
+import axios from "axios"
 import React, { useEffect } from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
+import { setPosition } from "../../Position/positionsSlice"
 import { userData } from "../userSlice"
 import "./ApplicantProfile.scss"
 
@@ -9,9 +11,9 @@ const ApplicantProfile = () => {
    const recruiterRoleId = "5695fbbd-4675-4b2a-b31d-603252c21c94"
    const userInfo = useSelector(userData)
    const navigate = useNavigate()
+   const dispatch = useDispatch()
 
    useEffect(() => {
-      console.log(userInfo)
       if (!userInfo?.data) {
          navigate('/')
       } else if (userInfo.data.role_id == recruiterRoleId) {
@@ -19,7 +21,20 @@ const ApplicantProfile = () => {
       }
    }, [])
 
-   
+   // ------------ HANDLERS ------------ \\
+
+   // go to position details on click
+   const goToPosition = async (positionId) => {
+      await axios.get(`https://aml-mysql-08-18-22-laravel-ip.herokuapp.com/api/positions/get-by-id/${positionId}`)
+         .then(resp => {
+            dispatch(setPosition(resp.data.data))
+         }).then(resp => {
+            navigate('/position-details')
+         }).catch(error => {
+            console.log(error)
+         })
+   }
+
    // ------------ RENDER FUNCTIONS ------------ \\
 
    // render a position card for each position stored in positionsList in redux slice
@@ -38,10 +53,34 @@ const ApplicantProfile = () => {
       }
    }
 
+   // render a position card for each position stored in positionsList in redux slice
+   const PositionList = () => {
+      if (userInfo?.data.positions.length > 0) {
+         return (
+            userInfo.data.positions.map((position, index) => (
+               <div className="userInfoItem" key={index}>
+                  <p className="userInfoHeading"> {position.title} </p>
+                  <p className="userInfoText"> Company: {position.company.name} </p>
+                  <p className="userInfoText"> Status: {position.application.status} </p>
+                  <button id="detailsButton" onClick={e => goToPosition(position.id)}>See details</button>
+               </div>
+            ))
+         )
+      } else {
+
+         return (
+            <div></div>
+         )
+      }
+   }
+
    // render personal info
    const UserInfo = () => {
       return (
          <div className="userInfo">
+            <div className="userInfoItem">
+               <p className="userInfoSection">MY PROFILE</p>
+            </div>
             <div className="userInfoItem">
                <p className="userInfoName">{userInfo.data.first_name} {userInfo.data.last_name}</p>
             </div>
@@ -59,7 +98,7 @@ const ApplicantProfile = () => {
                <p className="userInfoText">Title: {userInfo.data.title}</p>
                <p className="userInfoText">Skills:</p>
                <div className="skillContainer">
-                  <SkillList/>
+                  <SkillList />
                </div>
                <p className="userInfoText">Description:</p>
                <p className="userInfoText">{userInfo.data.description}</p>
@@ -68,10 +107,23 @@ const ApplicantProfile = () => {
       )
    }
 
+   // render positions that user is applying for
+   const UserPositions = () => {
+      return (
+         <div className="userInfo">
+            <div className="userInfoItem">
+               <p className="userInfoSection">MY APPLICATIONS</p>
+            </div>
+            <PositionList />
+         </div>
+      )
+   }
+
    return (
       <div id="ApplicantProfile">
          <div className="mainBox">
             <UserInfo />
+            <UserPositions />
          </div>
       </div>
    )
