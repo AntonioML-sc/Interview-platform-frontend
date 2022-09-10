55598dde881d7d436b87f97c000d95bfbab92a6a
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-10 15:40:57 +0200

    Feat(recruiter): render personal profile and user skills

--- src/containers/User/RecruiterProfile/RecruiterProfile.jsx
+++ src/containers/User/RecruiterProfile/RecruiterProfile.jsx
@@ -18,10 +18,58 @@ const RecruiterProfile = () => {
       }
    }, [])
 
+   // ------------ RENDER FUNCTIONS ------------ \\
+
+   // render a position card for each position stored in positionsList in redux slice
+   const SkillList = () => {
+      if (userInfo?.data.skills.length > 0) {
+         return (
+            userInfo.data.skills.map((skill, index) => (
+               <p key={index} className="skillTag">{skill.title}</p>
+            ))
+         )
+      } else {
+
+         return (
+            <div></div>
+         )
+      }
+   }
+
+   // render personal info
+   const UserInfo = () => {
+      return (
+         <div className="userInfo">
+            <div className="userInfoItem">
+               <p className="userInfoName">{userInfo.data.first_name} {userInfo.data.last_name}</p>
+            </div>
+            <div className="userInfoItem">
+               <p className="userInfoRole">Profile type: Recruiter</p>
+               <p className="userInfoText">Date of creation: {new Date(userInfo.data.created_at).toLocaleDateString()}</p>
+            </div>
+            <div className="userInfoItem">
+               <p className="userInfoHeading">CONTACT INFORMATION:</p>
+               <p className="userInfoText">Email: {userInfo.data.email}</p>
+               <p className="userInfoText">Phone: {userInfo.data.phone}</p>
+            </div>
+            <div className="userInfoItem">
+               <p className="userInfoHeading">PROFESSIONAL INFORMATION:</p>
+               <p className="userInfoText">Title: {userInfo.data.title}</p>
+               <p className="userInfoText">Skills:</p>
+               <div className="skillContainer">
+                  <SkillList />
+               </div>
+               <p className="userInfoText">Description:</p>
+               <p className="userInfoText">{userInfo.data.description}</p>
+            </div>
+         </div>
+      )
+   }
+
    return (
       <div id="RecruiterProfile">
          <div className="mainBox">
-            <p>MY PROFILE PAGE - RECRUITER</p>
+            <UserInfo />
          </div>
       </div>
    )
