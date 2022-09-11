8f513c7fd8050a61a951192d6e3b191519f0f2b1
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-11 17:33:14 +0200

    Feat(update): create update skill function in slice

--- src/containers/Skill/skillSlice.js
+++ src/containers/Skill/skillSlice.js
@@ -22,6 +22,14 @@ export const skillSlice = createSlice({
             errorMessage: ''
          }
       },
+      update: (state, action) => {
+         return {
+            ...state,
+            isError: false,
+            successMessage: 'Skill updated successfully',
+            errorMessage: ''
+         }
+      },
       logError: (state, action) => {
          return {
             ...state,
@@ -53,7 +61,27 @@ export const registerSkill = (title, description, token) => async (dispatch) => 
    }
 };
 
-export const { setSkill, setSkillList, logError, register } = skillSlice.actions
+export const updateSkill = (id, title, description, token) => async (dispatch) => {
+   try {
+      const body = {
+         title: title,
+         description: description
+      }
+      const config = {
+         headers: { "Authorization": `Bearer ${token}` }
+      }
+      const skill = await axios.put(`https://aml-mysql-08-18-22-laravel-ip.herokuapp.com/api/skills/update/${id}`, body, config);
+
+      let response = skill;
+      if (response.status === 200) {
+         dispatch(update());
+      }
+   } catch (error) {
+      dispatch(logError(error));
+   }
+};
+
+export const { setSkill, setSkillList, logError, register, update } = skillSlice.actions
 
 export const selectSkill = (state) => state.skill.skill
 export const selectSkillList = (state) => state.skill.skillList
