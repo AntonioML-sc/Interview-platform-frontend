64920c7f66ebb35b841876d0961f15aa38b0a163
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-11 17:39:05 +0200

    Feat(delete): create delete skill function in slice

--- src/containers/Skill/skillSlice.js
+++ src/containers/Skill/skillSlice.js
@@ -30,6 +30,14 @@ export const skillSlice = createSlice({
             errorMessage: ''
          }
       },
+      eliminate: (state, action) => {
+         return {
+            ...state,
+            isError: false,
+            successMessage: 'Skill deleted successfully',
+            errorMessage: ''
+         }
+      },
       logError: (state, action) => {
          return {
             ...state,
@@ -81,7 +89,22 @@ export const updateSkill = (id, title, description, token) => async (dispatch) =
    }
 };
 
-export const { setSkill, setSkillList, logError, register, update } = skillSlice.actions
+export const deleteSkill = (id, token) => async (dispatch) => {
+   try {
+      const config = {
+         headers: { "Authorization": `Bearer ${token}` }
+      }
+      const skill = await axios.delete(`https://aml-mysql-08-18-22-laravel-ip.herokuapp.com/api/skills/delete/${id}`, config);
+
+      let response = skill;
+      if (response.status === 200 || response.status === 204) {
+         dispatch(eliminate());
+      }
+   } catch (error) {
+      dispatch(logError(error));
+   }
+};
+export const { setSkill, setSkillList, logError, register, update, eliminate } = skillSlice.actions
 
 export const selectSkill = (state) => state.skill.skill
 export const selectSkillList = (state) => state.skill.skillList
