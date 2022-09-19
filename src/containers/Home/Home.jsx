bf9049498a3479cea999f714292c688cd14760c6
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-09-19 14:11:19 +0200

    Feat: divs structure and responsive design styles

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,12 +1,52 @@
 
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.scss"
 
 const Home = () => {
+   const navigate = useNavigate
 
-   return(
+   return (
       <div id="Home">
-         <div className="wellcomeMessageBox">
-            <p>HOME PAGE</p>
+         <div className="mainBox">
+            <div className="rowJoinUs">
+               <div className="joinUsImage">
+                  <img className="homeImg" src="" alt="Join us image" />
+               </div>
+               <div className="joinUsText">
+                  <p>JoinUs text</p>
+               </div>
+            </div>
+            <div className="rowPositions">
+               <div className="positionsImage">
+                  <img className="homeImg" src="" alt="Jobs image" />
+               </div>
+               <div className="positionsText">
+                  <p>Positions text</p>
+               </div>
+            </div>
+            <div className="rowSkills">
+               <div className="rowImages">
+                  <div className="skillImage1">
+                     <img className="homeImg" src="" alt="Skills image 1" />
+                  </div>
+                  <div className="skillImage2">
+                     <img className="homeImg" src="" alt="Tests image" />
+                  </div>
+               </div>
+               <div className="skillsText">
+                  <p>Skills text</p>
+               </div>
+            </div>
+            <div className="rowComingSoon">
+               <div className="comingSoonText">
+                  <p>Coming soon text</p>
+               </div>
+               <div className="rowImages">
+                  <div className="companiesImage"></div>
+                  <div className="usersImage"></div>
+               </div>
+            </div>
          </div>
       </div>
    )
